7ed554262246ea7199de622df797ed9c0d6e806d
AndreaGul <andreagulino99@gmail.com>
2024-01-30 22:20:38 +0100

    feat: tiene presente solo dei treni che partono da oggi in poi

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Train;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Carbon;
 
 class PageController extends Controller
 {
@@ -13,7 +13,7 @@ class PageController extends Controller
 
         
         
-        $trains = Train::all();
+        $trains = Train::all()->where('data_di_partenza','>=',Carbon::today());
 
         
         
